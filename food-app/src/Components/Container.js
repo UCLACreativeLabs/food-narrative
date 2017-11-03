aa7438ad3206ed3eb5810f72123e9e534d267d7b
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2017-11-02 19:18:13 -0700

    Adding components

---
+++ food-app/src/Components/Container.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Container.css';
+import Frame from './Frame';
+
+// This is the parent container
+
+class Container extends Component {
+
+  render() {
+    return (
+      <div className="Container">
+        <Frame name="Rishabh"/>
+        <Frame name="Rishabh"/>
+        <Frame name="Rishabh"/>
+      </div>
+    );
+  }
+}
+
+export default Container;
---
+++ food-app/src/Components/Frame.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import './Container.css';
+
+// This is the parent container
+
+class Frame extends Component<Props> {
+
+	// image: string (url)
+	// text: string 
+
+	render() {
+		return (
+		  <div className="Frame">
+		    
+		  </div>
+		);
+	}
+}
+
+export default Frame;
