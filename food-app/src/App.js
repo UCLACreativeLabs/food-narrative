f804695ac220fb93ff84658a001f71186af5d205
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2017-11-06 15:24:52 -0800

    Animating frames

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -2,8 +2,81 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Container from "./Components/Container";
+import frame1 from "./images/sample/1.png";
+import frame2 from "./images/sample/2.png";
+import frame3 from "./images/sample/3.png";
+import frame1a from "./images/sample/1a.jpg";
+import frame2a from "./images/sample/2a.gif";
+import frame3a from "./images/sample/3a.png";
+
+const state1 = {
+  containerId: "E1",
+  frames: [
+    {
+      image: frame1,
+      text: "First Scene"
+    },
+    {
+      image: frame2
+    },
+    {
+      image: frame3
+    }
+  ],
+  scene: null,
+  music: null
+};
+
+const state2 = {
+  containerId: "E2",
+  frames: [
+    {
+      image: frame1a,
+      text: "First Scene"
+    },
+    {
+      image: frame2a
+    },
+    {
+      image: frame3a
+    }
+  ],
+  scene: null,
+  music: null
+};
 
 class App extends Component {
+  state = {
+    containerId: "E1",
+    frames: [
+      {
+        image: frame1,
+        text: "First Scene"
+      },
+      {
+        image: frame2
+      },
+      {
+        image: frame3
+      }
+    ],
+    scene: null,
+    music: null
+  };
+
+  componentDidMount() {
+    let stateFlag = 1;
+    window.addEventListener("click", () => {
+      if(stateFlag === 1) {
+        this.setState(state2);
+        stateFlag = 2;
+      } else {
+        this.setState(state1);
+        stateFlag = 1;
+      }
+    });
+  }
+
   render() {
     return (
       // <div className="App">
@@ -15,7 +88,10 @@ class App extends Component {
       //     To get started, edit <code>src/App.js</code> and save to reload.
       //   </p>
       // </div>
-      <Container />
+      <Container
+        containerId={this.state.containerId}
+        frames={this.state.frames}
+      />
     );
   }
 }
--- food-app/src/Components/Container.js
+++ food-app/src/Components/Container.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React, { Component } from "react";
 import "./Container.css";
 import Frame from "./Frame";
@@ -5,12 +6,41 @@ import Frame from "./Frame";
 // This is the parent container
 
 class Container extends Component {
+  state = {
+    framesClass: "showFrames",
+  };
+
+  static propTypes = {
+    containerId: PropTypes.string,
+    frames: PropTypes.array,
+    scene: PropTypes.object,
+    music: PropTypes.string
+  };
+
+  fadeOut = () => {
+    this.setState({framesClass: "hideFrames"});
+    window.setTimeout(()=>{
+      this.setState({framesClass: "showFrames"});
+    }, 1000);
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.containerId !== this.props.containerId) {
+      this.fadeOut();
+    }
+  }
+
   render() {
+    console.log(this.props.containerId, this.props.frames);
+    const { containerId, frames, scene, music } = this.props;
     return (
       <div className="Container">
-        <Frame name="Rishabh" />
-        <Frame name="Rishabh" />
-        <Frame name="Rishabh" />
+        <div className={"Container__frames " + this.state.framesClass}>
+          <Frame image={frames[0].image} text={frames[0].text} />
+          <Frame image={frames[1].image} text={frames[1].text} />
+          <Frame image={frames[2].image} text={frames[2].text} />
+        </div>
+        <div className="Container__scene" />
       </div>
     );
   }
